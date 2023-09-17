37c90219314a5f4bd477f6b7aa5c847d93b12599
Fabian Schmitthenner <development@schmitthenner.eu>
2023-09-17 16:41:34 +0200

    Move LaTeX related files to subfolder src
    The root folder is getting crowded again, so let's bring back some structure

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
     name = "pf2e-cards";
-    src = "${./.}";
+    src = ./src;
     buildInputs = [ (texlive.combine { inherit (texlive) scheme-small latexmk; }) ];
     buildPhase = ''
       max_print_line=1000 latexmk -synctex=1 -interaction=nonstopmode -file-line-error -xelatex cards.tex a4print.tex
